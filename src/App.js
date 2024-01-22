2f59948226bb5c1adc14eb68e56b50c829bfdda1
Devang Kartik Suryavanshi <93068034+Devangkartik@users.noreply.github.com>
2024-01-22 17:40:34 +0530

    add files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-gray-800">
+      Hello world , Namaste React welcome once again ?
+    </div>
+  );
+}
+
+export default App;
