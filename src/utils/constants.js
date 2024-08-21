b723d71025d1cb121d0f9c925b1882c53310d6a9
Devang Kartik Suryavanshi <93068034+Devangkartik@users.noreply.github.com>
2024-08-21 13:23:20 +0530

    Updated .gitignore and utility files

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,20 +3,19 @@ export const userLogo = "https://avatars.githubusercontent.com/u/93068034?v=4";
 export const bgImage = "https://assets.nflxext.com/ffe/siteui/vlv3/16006346-87f9-4226-bc25-a1fb346a2b0c/9662d0fd-0547-4665-b887-771617268815/IN-en-20240115-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
 export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-    Authorization:'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTdjMGNlNTgxYjhiOGY3MTJiNzExZjE5ODBjN2ZhMyIsIm5iZiI6MTcyMzQ0MjI0Ny45OTM0ODQsInN1YiI6IjY1ZTgwNzg1OTYzODY0MDE4MWM5NGFkZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NGzWcMbdcRHiFEG7XpcLSST4umkVKtbOsYT4Z2qo7-U'
-    }
-  };
-  
-export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500"
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer ' + process.env.REACT_APP_TMDB_KEY
+  }
+};
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
 
 export const SUPPORTED_LANGUAGES = [
-  { identifier: "en", name:"English"},
-  { identifier: "hindi", name:"Hindi"},
-  { identifier: "spanish", name:"Spanish"},
-]
+{ identifier: "en", name: "English" },
+{ identifier: "hindi", name: "Hindi" },
+{ identifier: "spanish", name: "Spanish" }
+];
 
-
-export const GEMINI_SEARCH_KEY  = 'AIzaSyAeq6cmoRWZEvs_d2aCRdnDvcLwKJh5lNc'
+// export const GEMINI_SEARCH_KEY = process.env.REACT_APP_GEMINI_SEARCH_KEY;
--- src/utils/openAi.js
+++ src/utils/openAi.js
@@ -1,9 +1,14 @@
-import { GEMINI_SEARCH_KEY } from "./constants";
-
-
+// import { GEMINI_SEARCH_KEY } from "./constants";
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-// Access your API key as an environment variable (see "Set up your API key" above)
-export const genAI = new GoogleGenerativeAI(GEMINI_SEARCH_KEY);
+// Ensure the API key is correctly set
+if (!process.env.REACT_APP_GEMINI_SEARCH_KEY) {
+  throw new Error("API key is missing. Please set the GEMINI_SEARCH_KEY in your environment variables.");
+}
+// {
+//     apiKey: process.env.REACT_APP_GEMINI_SEARCH_KEY,
+//     dangerouslyAllowBrowser: true // Ensure this is necessary for your use case
+//   }
+export const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_SEARCH_KEY);
 
-export const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash",dangerouslyAllowBrowser : true});
+export const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
